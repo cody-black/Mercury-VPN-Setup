0436b2279601978f9b7c5d2c2eb49c3272c93935
cody-black <codyblack@ou.edu>
2020-09-05 10:15:30 -0500

    Re-added script because I deleted it on accident

---
+++ scripts/gce.sh
@@ -0,0 +1,1 @@
+wget https://git.io/angristanvpn -O openvpn-install.sh && chmod +x openvpn-install.sh && export PORT_CHOICE=2 PORT=443 APPROVE_IP=Y AUTO_INSTALL=y PROTOCOL_CHOICE=1 DNS=9 && ./openvpn-install.sh
